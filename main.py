68978dc20bcb84faf577fa15391b9c3355eff9a0
Diamantis Sellis <sellisd@gmail.com>
2024-02-06 23:35:05 +0100

    fix: prepare data to plot on map

--- main.py
+++ main.py
@@ -10,15 +10,20 @@ import requests
 def draw_map_with_arrows(locations):
     fig = plt.figure(figsize=(10, 5))
     ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    ax.add_feature(cfeature.LAND)
+    ax.add_feature(cfeature.LAND, facecolor='lightgray')
     ax.add_feature(cfeature.OCEAN)
     ax.add_feature(cfeature.COASTLINE)
     ax.add_feature(cfeature.BORDERS, linestyle=':')
     for i in range(len(locations)-1):
+        if None in locations[i] or None in locations[i+1]:  # skip if missing values
+            continue
         lons = [locations[i][1], locations[i+1][1]]
         lats = [locations[i][0], locations[i+1][0]]
-        ax.plot(lons, lats, 'o-', transform=ccrs.Geodetic(), color='red')
-        ax.arrow(lons[0], lats[0], lons[1]-lons[0], lats[1]-lats[0], color='blue', transform=ccrs.Geodetic(), width=0.5)
+        #ax.plot(lons, lats, 'o-', transform=ccrs.Geodetic(), color='red')
+        ax.set_global()
+        ax.scatter(locations[i][0], locations[i][1],color="red",transform=ccrs.Geodetic() )
+        #ax.arrow(lons[0], lats[0], lons[1]-lons[0], lats[1]-lats[0], color='blue', transform=ccrs.Geodetic(), width=0.5)
+        #ax.quiver(lons[0], lats[0], lons[1]-lons[0], lats[1]-lats[0], scale_units='xy', angles='xy', scale=1, color='blue', transform=ccrs.PlateCarree())
     plt.show()
 
 def geolocate(ip):
@@ -42,9 +47,11 @@ def traceroute(host):
       first_line = False
       continue
     data = parse_traceroute_line(line)
+    print(line)
     print(data)
     locations.append([data[1], data[2]])
-  draw_map_with_arrows(locations)
+  return(locations)
+
 
 def parse_traceroute_line(line):
   parts = line.split()
@@ -58,7 +65,7 @@ def parse_traceroute_line(line):
     if data['status'] == 'success':
       longitude = data['lon']
       latitude = data['lat']
-  return (hop_num, longitude, latitude, hop_ip)
+  return (hop_num, latitude, longitude, hop_ip)
 
 
 if __name__ == '__main__':
